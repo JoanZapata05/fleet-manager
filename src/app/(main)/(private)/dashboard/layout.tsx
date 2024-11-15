4456386a39b0aba91ea256895f405bbc1a12e43d
JoanZapata05 <zapatajoan05@gmail.com>
2024-11-15 13:34:00 -0500

    fix:

--- src/app/(main)/(private)/dashboard/layout.tsx
+++ src/app/(main)/(private)/dashboard/layout.tsx
@@ -1,7 +1,7 @@
 import { Roboto } from "next/font/google";
 import styles from "./layout.module.scss"; // Archivo CSS/Sass para los estilos
 import AuthGuard from "./guard/AuthGuard";
-import AsideContainer from "@/ui/organisms/AsideContainer.tsx/AsideContainer";
+
 
 const roboto = Roboto({
   subsets: ['latin'],
@@ -18,8 +18,6 @@ export default function PrivateLayout({
       <body className={`${roboto.className} ${styles.layoutContainer}`}>
         <div className={styles.mainContainer}>
           <AuthGuard><div className={styles.content}>{children}</div></AuthGuard>
-          <AsideContainer />
-          
         </div>
       </body>
     </html>
